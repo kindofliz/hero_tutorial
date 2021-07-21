f90857c458946d56f6b9bf4fe3e57fec1c054284
kindofliz <simanovicha@gmail.com>
2021-07-21 13:42:20 +0300

    Cleaned up the code; Added and example of an array and a loop in superpower section.

--- src/HeroApplication.java
+++ src/HeroApplication.java
@@ -1,3 +1,6 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HeroApplication {
@@ -7,11 +10,7 @@ public class HeroApplication {
         // Variable initialization
         String heroName;
         String heroCity;
-
-        String heroPower1, heroPower2, heroPower3;
-
         boolean isEvil;
-
         double heroSalary;
         String realName;
         String universe;
@@ -19,88 +18,132 @@ public class HeroApplication {
         // Variable assignment
         heroName = "DogoMom";
         heroCity = "Riga";
-        heroPower1 = "Lightning speed reaction";
-        heroPower2 = "Reads dog minds";
-        heroPower3 = " ";
+
+        //ADDED AN ARRAY (21.07.21)
+        String[] powersArray = {"Lightning speed reaction", "Reads dog minds", "Master cuddler"};
+
         isEvil = false;
-        heroSalary = 99.99;
+        heroSalary = 3050.99;
         realName = "Liza";
         universe = "Our";
 
         // Print out Hero information card
-        System.out.println("***********************");
-        System.out.println("*** HERO INFO ***");
-        System.out.println("***********************");
+        System.out.println();
+        System.out.println("***********************************");
+        System.out.println("************ HERO INFO ************");
+        System.out.println("***********************************");
 
+        System.out.println();
         System.out.println("Hero name: " + heroName);
-        System.out.println("Hero lives in : " + heroCity);
+        System.out.println("Hero lives in: " + heroCity);
 
-        System.out.println("-----------------------");
-        System.out.println("Hero Superpowers:");
-        System.out.println("---" + heroPower1);
-        System.out.println("---" + heroPower2);
-        System.out.println("---" + heroPower3);
+        System.out.println();
 
-        System.out.println("This super hero is evil? " + isEvil);
+        //ADDED A FOR LOOP (21.07.21)
+        System.out.println("HERO SUPERPOWERS:");
+        for (int i = 0; i < powersArray.length; i++) {
+            System.out.println(powersArray[i]);
+        }
 
-        System.out.println("This hero earns: " + heroSalary + " EUR ");
-        System.out.println(heroName + " hides her identity.");
-        System.out.println("Hero lives in " + universe + " Universe ");
 
-        System.out.println("***********************");
-        System.out.println("*** HERO INFO ***");
-        System.out.println("***********************");
+        System.out.println();
+        if (isEvil) {
+            System.out.println("This super hero is A VILLAIN!");
+        } else {
+            System.out.println("This super hero is not a villain!");
+        }
+
+        System.out.println();
+        System.out.println(heroName + " hides their true identity.");
+        System.out.println("This hero lives in " + universe + " Universe ");
+
+        System.out.println();
+        System.out.println("This hero earns: " + heroSalary + " EUR a month");
+        System.out.println();
 
 
         //How many cookies can a superhero purchase for her salary
         // 1 cookie costs = 1.29 euro
 
-        System.out.println("***********************");
-        System.out.println("*** FINANCIALS ***");
-        System.out.println("***********************");
+        System.out.println("***********************************");
+        System.out.println("********* HERO FINANCIALS *********");
+        System.out.println("***********************************");
+        System.out.println();
+
         int numOfCookies = (int) Math.floor(heroSalary / 1.29);
-        System.out.println("Hero can purchase " + numOfCookies + " cookies");
+        System.out.println("This hero can purchase " + numOfCookies + " cookies with their monthly salary.");
 
-        double averageDailySalary = Math.round(heroSalary / 30 * 100) / 100;
-        System.out.println("Hero's average daily salary is " + averageDailySalary + " euro");
+        double averageDailySalary = Math.round(heroSalary / 30 * 100) / 100d;
+        System.out.println("Hero's average daily salary is " + averageDailySalary + " euro.");
+        System.out.println();
 
-        //If a hero is a villain or a hero
-        if (isEvil) {
-            System.out.println("Hero is a villain. He is stealing cookies!");
-        } else {
-            System.out.println("The hero protects the city!");
-        }
 
-        heroCity = "Metropolis";
-
-        switch (heroCity) {
-            case "New York":
-                heroSalary = 2000;
-                break;
-            case "Riga":
-                heroSalary = 1200.91;
-                break;
-            case "Metropolis":
-                heroSalary = 1000000;
-                break;
-            default:
-                heroSalary = 100;
-        }
+//        heroCity = "Metropolis";
+//
+//        switch (heroCity) {
+//            case "New York":
+//                heroSalary = 2000;
+//                break;
+//            case "Riga":
+//                heroSalary = 1200.91;
+//                break;
+//            case "Metropolis":
+//                heroSalary = 1000000;
+//                break;
+//            default:
+//                heroSalary = 100;
+//        }
 
         // Task 2 - Superhero salary calculation
-        System.out.println("---- HERO SALARY PER DAY BY HOURLY RATES ----");
+        System.out.println("---- CALCULATE HERO SALARY PER DAY BY HOURLY RATES ----");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please insert the amount of hours the Hero works a day: ");
         int workingHoursInDay = scanner.nextInt();
 
-        if (workingHoursInDay<0 || workingHoursInDay>24) {
+        if (workingHoursInDay < 0 || workingHoursInDay > 24) {
             System.out.println("There can only be 24 hours in a day. Make sure you input the correct number.");
-        } else if (workingHoursInDay<=8) {
-            System.out.println("The hero salary is: " + workingHoursInDay * 10 + " Eur a day");
-        } else if (workingHoursInDay>8) {
-            System.out.println("The hero salary is: " + (80 + (workingHoursInDay-8)*15) + " Eur a day");
+        } else if (workingHoursInDay <= 8) {
+            System.out.println("This hero will earn: " + workingHoursInDay * 10 + " Eur a day");
+        } else if (workingHoursInDay > 8) {
+            System.out.println("This hero will earn: " + (80 + (workingHoursInDay - 8) * 15) + " Eur a day");
         }
+
+
+        // Class assignment (SuperHero application - work evaluation grades)
+        // 14.07.21
+        System.out.println();
+        System.out.println("***********************************");
+        System.out.println("******** HERO REPORT CARD *********");
+        System.out.println("***********************************");
+        System.out.println();
+
+        System.out.println("Please type in your work evaluation grade (A-G) in ALL CAPS: ");
+        char deedGrade = scanner.next().charAt(0);
+
+        switch (deedGrade) {
+            case 'A':
+            case 'B':
+                System.out.println("Perfect! You are so brave!");
+                break;
+            case 'C':
+                System.out.println("Good! But You can do better!");
+                break;
+            case 'D':
+            case 'E':
+                System.out.println("It is not good! You should choose your bad or good side!");
+                break;
+            case 'F':
+                System.out.println("Bad, are you even trying to be good?");
+                break;
+            case 'G':
+                System.out.println("Bad, you are a true villain!");
+                break;
+            default:
+                System.out.println("A grade you tried to input does not exist!");
+
+        }
+
 
 
     }
